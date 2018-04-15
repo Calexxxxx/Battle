1a8bde2fba99eb58c4c3bf06dea61ce658693b98
Calexxxxx <pasm28@hotmail.nl>
2018-04-15 18:12:45 +0200

    Feat: Production build optimization
    Signed-off-by: Calexxxxx <pasm28@hotmail.nl>

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const   path = require('path'),
         HtmlWebpackPlugin = require('html-webpack-plugin'),
-        webpack = require('webpack');
+    webpack = require('webpack'),
+    CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const config = {
     entry: ['whatwg-fetch', 'babel-polyfill', './app/index.js'],
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.[hash].js',
+        filename: 'main.bundle.js',
         publicPath: '/'
     },
     module: {
@@ -21,6 +22,7 @@ const config = {
         hot: true,
         contentBase: './dist'
     },
+    performance: {hints: false},
     plugins: [
         new HtmlWebpackPlugin({
             template: 'app/index.html'
@@ -31,13 +33,20 @@ const config = {
 
 if (process.env.NODE_ENV === 'production') {
     config.plugins.push(
+        new CleanWebpackPlugin(['dist']),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common' // Specify the common bundle's name.
+        }),
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
             }
         }),
-        new webpack.optimize.UglifyJsPlugin()
-    )
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true
+        })
+    );
+    config.devtool = 'source-map';
 }
 
 module.exports = config;
